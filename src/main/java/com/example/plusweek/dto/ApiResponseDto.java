f0f2fc73cdfd8c3739e8fed8ccbc795bd41a9e30
JungHoon <meslee70@gmail.com>
2023-07-28 18:17:43 +0900

    ApiResponse 추가

---
+++ src/main/java/com/example/plusweek/dto/ApiResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.plusweek.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponseDto {
+    private String msg;
+    private Integer statusCode;
+
+    public ApiResponseDto(String msg, Integer statusCode) {
+        this.msg = msg;
+        this.statusCode = statusCode;
+    }
+}
